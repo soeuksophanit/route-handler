bb539c80248bc27c688372e95523dccb0ef25e61
sophanit <sophanit@sophanits-MacBook-Pro.local>
2024-04-02 11:56:21 +0700

    adding post and get to order

---
+++ app/api/order/route.ts
@@ -0,0 +1,66 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { response } from "../category/route";
+
+interface Order {
+  product_id: number;
+  customer_id: number;
+  order_qty: number;
+}
+
+export const GET = async (req: NextRequest) => {
+  const orderList = await prisma.order.findMany();
+  if (orderList.length) {
+    return NextResponse.json(
+      response(200, `Get All ${orderList.length} order lists`, orderList),
+      { status: 200 }
+    );
+  }
+  return NextResponse.json(
+    response(200, `There is no order list right now`, []),
+    { status: 200 }
+  );
+};
+
+export const POST = async (req: NextRequest) => {
+  const { customer_id, order_qty, product_id }: Order = await req.json();
+  const customer = await prisma.customers.findUnique({
+    where: {
+      customer_id,
+    },
+  });
+  const product = await prisma.products.findUnique({
+    where: { product_id },
+  });
+  if (customer && product) {
+    if (customer_id && order_qty && product_id) {
+      const neworder = await prisma.order.create({
+        data: {
+          product_id,
+          customer_id,
+          order_qty,
+          order_date: new Date().toISOString(),
+          order_total: product.price * order_qty,
+        },
+      });
+
+      return NextResponse.json(
+        response(
+          201,
+          `Order ID ${neworder.order_id} was add successfully`,
+          neworder
+        ),
+        { status: 201 }
+      );
+    }
+    return NextResponse.json(
+      response(501, `Some field might be empty or Invalid`, []),
+      { status: 501 }
+    );
+  }
+
+  return NextResponse.json(
+    response(200, `It seem like ther is no Customer and Product`, []),
+    { status: 200 }
+  );
+};
